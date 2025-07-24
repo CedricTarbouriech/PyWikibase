9230c835c62d0e9566655787a0a8ad308f98b496
Cédric Tarbouriech <cedtrabou@gmail.com>
2025-07-24 13:13:07 +0200

    Separated models in different files

--- wikibase/models/base.py
+++ wikibase/models/base.py
@@ -16,10 +16,6 @@ class InheritanceForeignKey(models.ForeignKey):
     forward_related_accessor_class = InheritanceForwardManyToOneDescriptor
 
 
-def positive(v: float | int) -> None:
-    if v < 0:
-        raise exceptions.ValidationError(f'expected v >= 0, got {v}', 'positive')
-
 
 class IriField(models.TextField):
     pass
@@ -30,8 +26,7 @@ class Value(models.Model):
 
 
 class Entity(Value):
-    identifier = IriField()
-
+    pass
 
 class Datatype(Entity):
     class_name = models.CharField(max_length=50)
@@ -56,19 +51,8 @@ class Datatype(Entity):
 
 
 class DescribedEntity(Entity):
-    labels = models.ForeignKey('MultilingualTextValue', on_delete=models.PROTECT, related_name='labelled_items')
-    descriptions = models.ForeignKey('MultilingualTextValue', on_delete=models.PROTECT, related_name='described_items')
-    aliases = models.ForeignKey('MultilingualMultiTextValue', on_delete=models.PROTECT, related_name='aliased_items')
-
     def get_labels(self):
         return self.labels.monolingualtextvalue_set.all()
-
-    def save(self, *args, **kwargs):
-        if self._state.adding:
-            self.labels = MultilingualTextValue.objects.create()
-            self.descriptions = MultilingualTextValue.objects.create()
-            self.aliases = MultilingualMultiTextValue.objects.create()
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return f': <{self.get_labels()[0]}>' if self.get_labels() else ''
@@ -145,90 +129,6 @@ class Statement(models.Model):
 class ReferenceRecord(models.Model):
     statement = models.ForeignKey(Statement, on_delete=models.CASCADE, related_name='references')
     snak = InheritanceForeignKey(Snak, on_delete=models.PROTECT)
-
-
-class DataValue(Value):
-    pass
-
-
-class StringValue(DataValue):
-    value = models.TextField()
-
-    def __str__(self):
-        return self.value
-
-
-class UrlValue(DataValue):
-    value = models.TextField()
-
-    def __str__(self):
-        return self.value
-
-
-# Removed lower and upper bound, and created subclass with unit.
-class QuantityValue(DataValue):
-    number = models.FloatField()
-
-    def __str__(self):
-        return f"{self.number}"
-
-
-class UnitQuantityValue(QuantityValue):
-    unit = models.ForeignKey(Item, on_delete=models.PROTECT)
-
-
-class TimeValue(DataValue):
-    time = models.CharField(max_length=40)
-    timezone = models.IntegerField()  # Offset from UTC in minutes
-    # Precision for "before" and after "fields"
-    precision = models.IntegerField(choices=(
-        (0, 'Billion years'),
-        (1, '100 million years'),
-        (2, '10 million years'),
-        (3, 'Million years'),
-        (4, '100 thousand years'),
-        (5, '10 thousand years'),
-        (6, 'Thousand years'),
-        (7, 'Century'),
-        (8, 'Decade'),
-        (9, 'Year'),
-        (10, 'Month'),
-        (11, 'Day'),
-    ), blank=True, null=True)
-    after = models.IntegerField(blank=True, null=True)
-    before = models.IntegerField(blank=True, null=True)
-    calendar_model = IriField()
-
-
-class GlobeCoordinatesValue(DataValue):
-    latitude = models.DecimalField(max_digits=11, decimal_places=9)
-    longitude = models.DecimalField(max_digits=12, decimal_places=9)
-    precision = models.DecimalField(max_digits=12, decimal_places=9, validators=[positive])
-    # Changed IRI to Item
-    unit = models.ForeignKey(Item, on_delete=models.PROTECT)
-
-
-class MultilingualTextValue(DataValue):
-    pass
-
-
-class MonolingualTextValue(DataValue):
-    lang_code = models.CharField(max_length=3)
-    value = models.TextField()
-    multilingual_value = models.ForeignKey(MultilingualTextValue, on_delete=models.CASCADE, blank=True, null=True)
-
-    def __str__(self):
-        return f'({self.lang_code}) {self.value}'
-
-
-class MultilingualMultiTextValue(DataValue):
-    pass
-
-
-class MonolingualMultiTextValue(DataValue):
-    lang_code = models.CharField(max_length=3)
-    values = models.TextField()  # FIXME
-    multilingual_value = models.ForeignKey(MultilingualMultiTextValue, on_delete=models.CASCADE, blank=True, null=True)
 
 
 class PropertyMapping(models.Model):
---
+++ wikibase/models/datatypes.py
@@ -0,0 +1,88 @@
+from django.core import exceptions
+from django.db import models
+
+from .base import Value, Item, DescribedEntity
+
+
+def positive(v: float | int) -> None:
+    if v < 0:
+        raise exceptions.ValidationError(f'expected v >= 0, got {v}', 'positive')
+
+
+class DataValue(Value):
+    pass
+
+
+class StringValue(DataValue):
+    value = models.TextField()
+
+    def __str__(self):
+        return self.value
+
+
+class UrlValue(DataValue):
+    value = models.TextField()
+
+    def __str__(self):
+        return self.value
+
+
+# Removed lower and upper bound, and created subclass with unit.
+class QuantityValue(DataValue):
+    number = models.FloatField()
+    lower = models.FloatField(blank=True, null=True)
+    upper = models.FloatField(blank=True, null=True)
+    unit = models.ForeignKey(Item, on_delete=models.PROTECT, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.number}"
+
+
+class TimeValue(DataValue):
+    time = models.CharField(max_length=40)
+    timezone = models.IntegerField()  # Offset from UTC in minutes
+    # Precision for "before" and after "fields"
+    precision = models.IntegerField(choices=(
+        (0, 'Billion years'),
+        (1, '100 million years'),
+        (2, '10 million years'),
+        (3, 'Million years'),
+        (4, '100 thousand years'),
+        (5, '10 thousand years'),
+        (6, 'Thousand years'),
+        (7, 'Century'),
+        (8, 'Decade'),
+        (9, 'Year'),
+        (10, 'Month'),
+        (11, 'Day'),
+    ), blank=True, null=True)
+    after = models.IntegerField(blank=True, null=True)
+    before = models.IntegerField(blank=True, null=True)
+    calendar_model = models.ForeignKey(Item, on_delete=models.PROTECT)
+
+
+class GlobeCoordinatesValue(DataValue):
+    latitude = models.DecimalField(max_digits=11, decimal_places=9)
+    longitude = models.DecimalField(max_digits=12, decimal_places=9)
+    precision = models.DecimalField(max_digits=12, decimal_places=9, validators=[positive])
+    # Changed IRI to Item
+    unit = models.ForeignKey(Item, on_delete=models.PROTECT)
+
+
+class MonolingualTextValue(DataValue):
+    lang_code = models.CharField(max_length=3)
+    value = models.TextField()
+
+    def __str__(self):
+        return f'({self.lang_code}) {self.value}'
+
+class Label(MonolingualTextValue):
+    described_entity = models.ForeignKey(DescribedEntity, on_delete=models.PROTECT)
+
+
+class Description(MonolingualTextValue):
+    described_entity = models.ForeignKey(DescribedEntity, on_delete=models.PROTECT)
+
+
+class Alias(MonolingualTextValue):
+    described_entity = models.ForeignKey(DescribedEntity, on_delete=models.PROTECT)
