0f98ae9f7dfc186f2864af5c26a93ff766599c95
Cédric Tarbouriech <cedtrabou@gmail.com>
2025-07-30 13:46:37 +0200

    Added filters to mappings in admin interface

--- wikibase/admin.py
+++ wikibase/admin.py
@@ -1,11 +1,45 @@
+from django import forms
 from django.contrib import admin
 
-from wikibase.models import PropertyMapping, ItemMapping, Datatype
+from wikibase.models import PropertyMapping, Datatype, Property
+from .models import ItemMapping, Item
 
 
-# Register your models here.
+class ItemMappingForm(forms.ModelForm):
+    class Meta:
+        model = ItemMapping
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        used_items = ItemMapping.objects.values_list('item_id', flat=True)
+
+        if self.instance and self.instance.pk:
+            used_items = used_items.exclude(pk=self.instance.item_id)
+
+        self.fields['item'].queryset = Item.objects.exclude(pk__in=used_items)
+
+
+class PropertyMappingForm(forms.ModelForm):
+    class Meta:
+        model = PropertyMapping
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        used_properties = PropertyMapping.objects.values_list('property_id', flat=True)
+
+        if self.instance and self.instance.pk:
+            used_properties = used_properties.exclude(pk=self.instance.property_id)
+
+        self.fields['property'].queryset = Property.objects.exclude(pk__in=used_properties)
+
+
 @admin.register(PropertyMapping)
 class PropertyMappingAdmin(admin.ModelAdmin):
+    form = PropertyMappingForm
     list_display = ["key", "property"]
     search_fields = ["key", "property__display_id"]
     ordering = ["key"]
@@ -13,6 +47,7 @@ class PropertyMappingAdmin(admin.ModelAdmin):
 
 @admin.register(ItemMapping)
 class ItemMappingAdmin(admin.ModelAdmin):
+    form = ItemMappingForm
     list_display = ["key", "item"]
     search_fields = ["key", "item__display_id"]
     ordering = ["key"]
